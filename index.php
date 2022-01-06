a607a32274867fca598420d4dd524eeaa0d85f46
jeremybrohan <97220762+jeremybrohan@users.noreply.github.com>
2022-01-06 10:18:08 +0100

    Creation du premier fichier index.php
    Construction du Hello World
    Commentaires
    Explications  du Hello World

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+echo 'Hello World';
+// 1 commentaire = 1 ligne qui ne s'execute pas
+
+/*
+1 bloc de commentaires
+*/
+
+// echo me permet d'ecrire le document HTML qui sera afficher par la navigateur
+
+//'Hello World ' est une chaine de caracteres 
+//en PHP, les ligne finnise par ;
